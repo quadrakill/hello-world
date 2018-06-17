2bbd18b1733434c4752a960f5d56224a85dd306c
quadrakill <37108301+quadrakill@users.noreply.github.com>
2018-06-17 15:25:00 +0800

    Update Units.cpp

--- Units.cpp
+++ Units.cpp
@@ -4,18 +4,3 @@
 //长度单位：m, km, ft, in,......
 //其它单位：md, cp,......
 //还有很多单位转换，有些单位是其它单位组合出来的
-#include<iostream>
- using namespace std;
- const double inch=0.39;
- const double foot=0.03;
- int main()
- {
-   int cm;
-  double myinch,myfoot;
-   cout<<"输入你的身高（CM）";
-  cin>>cm;
-  myinch=inch*cm;
-  myfoot=foot*cm;
-  cout<<"你的身高是"<<cm<<"cm "<<"相当于 "<<myinch<<"相当于"<<myfoot<<endl;
-  return 0;
- }
